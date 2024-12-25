8298b8dcb906752038fef3329948b34912ec8c95
Aurora Rossi <aurora98rossi@gmail.com>
2024-12-25 12:39:17 +0100

    `Hands-on` pluto to literate

--- GraphNeuralNetworks/docs/make_tutorials_literate.jl
+++ GraphNeuralNetworks/docs/make_tutorials_literate.jl
@@ -2,5 +2,8 @@ using Literate
 
 ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 
+Literate.markdown("src_tutorials/introductory_tutorials/gnn_intro.jl", 
+                  "src/tutorials/"; execute = true)
+
 Literate.markdown("src_tutorials/introductory_tutorials/temporal_graph_classification.jl", 
                   "src/tutorials/"; execute = true)
