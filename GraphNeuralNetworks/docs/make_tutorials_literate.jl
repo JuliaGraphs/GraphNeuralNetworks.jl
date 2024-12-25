d7e036f9fd5a803febd11764aa375b4589a71eed
Aurora Rossi <aurora98rossi@gmail.com>
2024-12-25 20:10:21 +0100

    Add graph classification literate and some fixes

--- GraphNeuralNetworks/docs/make_tutorials_literate.jl
+++ GraphNeuralNetworks/docs/make_tutorials_literate.jl
@@ -8,5 +8,8 @@ Literate.markdown("src_tutorials/introductory_tutorials/gnn_intro.jl",
 Literate.markdown("src_tutorials/introductory_tutorials/node_classification.jl",
                  "src/tutorials/"; execute = true)
 
+Literate.markdown("src_tutorials/introductory_tutorials/graph_classification.jl", 
+                  "src/tutorials/"; execute = true)
+
 Literate.markdown("src_tutorials/introductory_tutorials/temporal_graph_classification.jl", 
                   "src/tutorials/"; execute = true)
