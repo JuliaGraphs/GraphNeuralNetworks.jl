2f5a00a96cee4784e12a89a0212c1a7a18e2fff6
Aurora Rossi <aurora98rossi@gmail.com>
2024-12-25 18:47:45 +0100

    Add markdown

--- GraphNeuralNetworks/docs/make_tutorials_literate.jl
+++ GraphNeuralNetworks/docs/make_tutorials_literate.jl
@@ -5,5 +5,8 @@ ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 Literate.markdown("src_tutorials/introductory_tutorials/gnn_intro.jl", 
                   "src/tutorials/"; execute = true)
 
+Literate.markdown("src_tutorials/introductory_tutorials/node_classification.jl",
+                 "src/tutorials/"; execute = true)
+
 Literate.markdown("src_tutorials/introductory_tutorials/temporal_graph_classification.jl", 
                   "src/tutorials/"; execute = true)
