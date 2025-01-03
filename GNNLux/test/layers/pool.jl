4adef57b83440281a1326ca8c169a43e5f2e7c5a
Aurora Rossi <aurora98rossi@gmail.com>
2025-01-03 14:22:36 +0100

    Fix

--- GNNLux/test/layers/pool.jl
+++ GNNLux/test/layers/pool.jl
@@ -8,13 +8,13 @@
         x = randn(rng, Float32, in_dims, 10)
 
         @testset "GlobalPool" begin
-            l = GNNLux.GlobalPool(mean)
+            l = GlobalPool(mean)
             test_lux_layer(rng, l, g, x, sizey=(in_dims,1))
         end
         @testset "GlobalAttentionPool" begin
             fgate = Dense(in_dims, 1)
             ffeat = Dense(in_dims, in_dims)
-            l = GNNLux.GlobalAttentionPool(fgate, ffeat)
+            l = GlobalAttentionPool(fgate, ffeat)
             test_lux_layer(rng, l, g, x, sizey=(in_dims,1), container=true)
         end
     end
