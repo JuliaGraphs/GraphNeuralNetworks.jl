7c27440deece70a871f5c720874f50900485c333
CarloLucibello <carlo.lucibello@gmail.com>
2025-12-22 09:32:56 +0100

    fix gpu

--- GNNlib/test/msgpass.jl
+++ GNNlib/test/msgpass.jl
@@ -169,6 +169,7 @@ end
     @testset "copy_xj +" begin
         for g in TEST_GRAPHS
             # broken = get_graph_type(g) == :sparse
+            broken = false
             f(g, x) = propagate(copy_xj, g, +, xj = x)
             @test test_gradients(
                 f, g, g.x; test_gpu=true, test_grad_f=false, compare_finite_diff=false
@@ -179,6 +180,7 @@ end
     @testset "copy_xj mean" begin
         for g in TEST_GRAPHS
             # broken = get_graph_type(g) == :sparse
+            broken = false
             f(g, x) = propagate(copy_xj, g, mean, xj = x)
             @test test_gradients(
                 f, g, g.x; test_gpu=true, test_grad_f=false, compare_finite_diff=false
@@ -189,6 +191,7 @@ end
     @testset "e_mul_xj +" begin
         for g in TEST_GRAPHS
             # broken = get_graph_type(g) == :sparse
+            broken = false
             e = rand(Float32, size(g.x, 1), g.num_edges)
             f(g, x, e) = propagate(e_mul_xj, g, +; xj = x, e)
             @test test_gradients(
